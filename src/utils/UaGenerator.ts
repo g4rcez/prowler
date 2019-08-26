359968c1b8887eb8f762bae04e1d6affbec29901
Garcez <allan.f.garcez@gmail.com>
2019-08-26 00:49:23 -0300

    Future user agent generator

---
+++ src/utils/UaGenerator.ts
@@ -0,0 +1,6 @@
+const UA = [
+	"Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Mobile Safari/537.36"
+];
+
+// !ToDo: Create a random get for UA
+export default () => UA[0];
